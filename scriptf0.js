74fc3408100f6af9001468ea989d12b8f54a3bd1
Yashvi <yashvi.mervana@vit.edu.in>
2022-10-05 13:00:37 +0530

    con exe

---
+++ scriptf0.js
@@ -0,0 +1,197 @@
+//jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+$(".next").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+	
+	//activate next step on progressbar using the index of next_fs
+	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+	
+	//show the next fieldset
+	next_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale current_fs down to 80%
+			scale = 1 - (1 - now) * 0.2;
+			//2. bring next_fs from the right(50%)
+			left = (now * 50)+"%";
+			//3. increase opacity of next_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({
+        'transform': 'scale('+scale+')',
+        'position': 'absolute'
+      });
+			next_fs.css({'left': left, 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+$(".previous").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	previous_fs = $(this).parent().prev();
+	
+	//de-activate current step on progressbar
+	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	
+	//show the previous fieldset
+	previous_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale previous_fs from 80% to 100%
+			scale = 0.8 + (1 - now) * 0.2;
+			//2. take current_fs to the right(50%) - from 0%
+			left = ((1-now) * 50)+"%";
+			//3. increase opacity of previous_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({'left': left});
+			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+$(".submit").click(function(){
+	return false;
+})
+
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+	"particles": {
+	  "number": {
+		"value": 90,
+		"density": {
+		  "enable": true,
+		  "value_area": 800
+			
+		}
+		
+	 
+	  },
+	  "shape": {
+		"type": "circle",
+		"stroke": {
+		  "width": 2,
+		  "color": "#fff"
+		},
+		"polygon": {
+		  "nb_sides": 7
+		},
+		"image": {
+		  "src": "img/github.svg",
+		  "width": 100,
+		  "height": 100
+		}
+	  },
+	  "opacity": {
+		"value": 1,
+		"random": false,
+		"anim": {
+		  "enable": false,
+		  "speed": 3,
+		  "opacity_min": 0.1,
+		  "sync": false
+		}
+	  },
+	  "size": {
+		"value": 3,
+		"random": true,
+		"anim": {
+		  "enable": false,
+		  "speed": 20,
+		  "size_min": 0.1,
+		  "sync": false
+		}
+	  },
+	  "line_linked": {
+		"enable": true,
+		"distance": 250,
+		"color": "fff",
+		"opacity": 0.4,
+		"width": 1
+	  },
+	  "move": {
+		"enable": true,
+		"speed": 3,
+		"direction": "none",
+		"random": true,
+		"straight": false,
+		"out_mode": "out",
+		"bounce": false,
+		"attract": {
+		  "enable": false,
+		  "rotateX": 600,
+		  "rotateY": 1200
+		}
+		
+	  }
+	},
+	"interactivity": {
+	  "detect_on": "canvas",
+	  "events": {
+		"onhover": {
+		  "enable": true,
+		  "mode": "grab"
+		},
+		"onclick": {
+		  "enable": true,
+		  "mode": "push"
+		  
+		},
+		"resize": true
+	  },
+	  "modes": {
+		"grab": {
+		  "distance": 140,
+		  "line_linked": {
+			"opacity": 1
+		  }
+		},
+		"bubble": {
+		  "distance": 300,
+		  "size": 70,
+		  "duration": 2,
+		  "opacity": 8,
+		  "speed": 3
+		},
+		"repulse": {
+		  "distance": 500,
+		  "duration": 0.4
+		},
+		"push": {
+		  "particles_nb": 4
+		},
+		"remove": {
+		  "particles_nb": 2
+		}
+	  }
+	},
+	"retina_detect": true
+   
+  });
